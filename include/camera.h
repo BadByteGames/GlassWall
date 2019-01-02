ae91c1cb13dc0cc01fd6ea6a25dddc4244f6d629
armedturret <rjwhitenh@outlook.com>
2019-01-02 12:06:04 -0500

    Created camera class to calculate matrices

--- include/camera.h
+++ include/camera.h
@@ -8,11 +8,62 @@ namespace GW{
 		public:
 			Camera();
 			~Camera();
-
+			
+			//sets absolute position in world
 			void setAbsolutePosition(const glm::vec3& position);
+			
+			//set rotation values (in degrees)
+			void setRotation(const glm::vec3& rotations);
 
+			//set rotation values (in degrees)
+			void setRotation(float x, float y, float z);
+
+			//sets target
+			void setTarget(const glm::vec3& target);
+
+			//sets up vector
+			void setUpVector(const glm::vec3& up);
+
+			/*sets camera ortho value
+			*true = orthographic
+			*false = perspective
+			*/
+			void setOrthopgraphic(const bool& value);
+
+			//update targets
+			void update();
+
+			//set window dimensions
+			void setDimensions(const int& width, const int& height);
+
+			//calculates necessary matrices for graphics
+			glm::mat4 getViewMatrix();
+			glm::mat4 getProjectionMatrix();
+
+			//sets the field of viw
+			void setFOV(const float& fov);
 		private:
+			//stores camera position
 			glm::vec3 m_position;
+			//stores the location for the camera to look at
+			glm::vec3 m_target;
+			//stores the "up" direction of the camera
+			glm::vec3 m_up;
+			//stores x, y, and z rotations stored in radians
+			glm::vec3 m_rotations;
+
+			//is camera orthographic
+			bool m_orthographic;
+
+			//is camera in look at mode
+			bool m_lookat;
+
+			//store width and height for perspective calculations
+			int m_width;
+			int m_height;
+
+			//field of vision
+			float m_fov;
 		};
 	}
 }
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,8 +1,20 @@
 #include "camera.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+const float PI = 3.14159265359f;
+
 GW::RenderEngine::Camera::Camera()
 {
 	m_position = glm::vec3(0.0f);
+	m_up = glm::vec3(0.0f, 1.0f, 0.0f);
+	m_target = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	m_orthographic = false;
+	m_lookat = false;
+
+	m_width = 1920;
+	m_height = 1080;
+	m_fov = 60.0f;
 }
 
 GW::RenderEngine::Camera::~Camera()
@@ -11,4 +23,83 @@ GW::RenderEngine::Camera::~Camera()
 
 void GW::RenderEngine::Camera::setAbsolutePosition(const glm::vec3 & position)
 {
+	m_position = position;
+}
+
+void GW::RenderEngine::Camera::setRotation(const glm::vec3& rotations)
+{
+	m_rotations = glm::vec3((rotations.x * PI) / 180.0f, (rotations.y * PI) / 180.0f, (rotations.z * PI) / 180.0f);
+}
+
+void GW::RenderEngine::Camera::setRotation(float x, float y, float z)
+{
+	m_rotations = glm::vec3((x * PI) / 180.0f, (y * PI) / 180.0f, (z * PI) / 180.0f);
+}
+
+void GW::RenderEngine::Camera::setTarget(const glm::vec3 & target)
+{
+	m_target = target;
+}
+
+void GW::RenderEngine::Camera::setUpVector(const glm::vec3 & up)
+{
+	m_up = up;
+}
+
+void GW::RenderEngine::Camera::setOrthopgraphic(const bool & value)
+{
+	m_orthographic = value;
+}
+
+void GW::RenderEngine::Camera::update()
+{
+	//only update if not in m_lookat mode
+	if (!m_lookat) {
+		m_target.x = cos(m_rotations.x) * sin(m_rotations.y);
+		m_target.y = cos(m_rotations.x) * cos(m_rotations.y);
+		m_target.z = sin(m_rotations.x);
+		
+		//add in location
+		m_target += m_position;
+
+		m_up.x = cos(m_rotations.x + (90.0f * PI) / 180.0f) * sin(m_rotations.z);
+		m_up.y = cos(m_rotations.x + (90.0f * PI) / 180.0f) * cos(m_rotations.z);
+		m_up.z = sin(m_rotations.x + (90.0f * PI) / 180.0f);
+
+		m_up += m_position;
+	}
+}
+
+void GW::RenderEngine::Camera::setDimensions(const int & width, const int & height)
+{
+	m_width = width;
+	m_height = height;
+}
+
+glm::mat4 GW::RenderEngine::Camera::getViewMatrix()
+{
+	glm::mat4 viewMatrix = glm::lookAt(m_position, m_target, m_up);
+	
+	return viewMatrix;
+}
+
+glm::mat4 GW::RenderEngine::Camera::getProjectionMatrix()
+{
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	float aspectRatio = (float)m_width / (float)m_height;
+
+	if (m_orthographic) {
+		projection = glm::ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f, 0.1f, 1000.0f);
+	}
+	else {
+		projection = glm::perspective(m_fov, aspectRatio, 0.1f, 1000.0f);
+	}
+
+	return projection;
+}
+
+void GW::RenderEngine::Camera::setFOV(const float & fov)
+{
+	m_fov = fov;
 }
